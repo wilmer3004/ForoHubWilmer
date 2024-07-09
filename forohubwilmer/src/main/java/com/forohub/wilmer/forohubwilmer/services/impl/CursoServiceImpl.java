00950e84aebdbc2f4c70d3a2fa97399988a9c4bc
wilmer3004 <131203913+wilmer3004@users.noreply.github.com>
2024-07-09 17:06:27 -0500

    created crud of topico and curso

--- forohubwilmer/src/main/java/com/forohub/wilmer/forohubwilmer/services/impl/CursoServiceImpl.java
+++ forohubwilmer/src/main/java/com/forohub/wilmer/forohubwilmer/services/impl/CursoServiceImpl.java
@@ -4,10 +4,12 @@ import com.forohub.wilmer.forohubwilmer.dtos.DatosCurso;
 import com.forohub.wilmer.forohubwilmer.models.Curso;
 import com.forohub.wilmer.forohubwilmer.repositories.CursoRepository;
 import com.forohub.wilmer.forohubwilmer.services.CursoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,28 +19,38 @@ public class CursoServiceImpl implements CursoService {
     @Autowired
     private CursoRepository cursoRepository;
 
+    private final ModelMapper modelMapper = new ModelMapper();
+
     @Override
     public List<DatosCurso> listCurso() {
-        return null;
+
+        List<Curso> cursoList = cursoRepository.findAll();
+        List<DatosCurso> datosCurso = new ArrayList<>();
+
+        cursoList.forEach(curso -> datosCurso.add(modelMapper.map(curso, DatosCurso.class)));
+
+        return datosCurso;
     }
 
     @Override
     public DatosCurso findByIdCurso(Long id) {
-        return null;
+        return modelMapper.map(cursoRepository.findById(id), DatosCurso.class);
     }
 
     @Override
     public void addCurso(DatosCurso datosCurso) {
-
+        cursoRepository.save(modelMapper.map(datosCurso, Curso.class));
     }
 
     @Override
     public void updateCurso(DatosCurso datosCurso, Long id) {
-
+        Curso curso = modelMapper.map(datosCurso, Curso.class);
+        curso.setId(id);
+        cursoRepository.save(curso);
     }
 
     @Override
     public void deleteCurso(Long id) {
-
+        cursoRepository.deleteById(id);
     }
 }
--- forohubwilmer/src/main/java/com/forohub/wilmer/forohubwilmer/services/impl/TopicoServiceImpl.java
+++ forohubwilmer/src/main/java/com/forohub/wilmer/forohubwilmer/services/impl/TopicoServiceImpl.java
@@ -4,9 +4,11 @@ import com.forohub.wilmer.forohubwilmer.dtos.DatosTopico;
 import com.forohub.wilmer.forohubwilmer.models.Topico;
 import com.forohub.wilmer.forohubwilmer.repositories.TopicoRepository;
 import com.forohub.wilmer.forohubwilmer.services.TopicoService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,28 +18,39 @@ public class TopicoServiceImpl implements TopicoService {
     @Autowired
     private TopicoRepository topicoRepository;
 
+    private final ModelMapper modelMapper = new ModelMapper();
+
     @Override
     public List<DatosTopico> listTopico() {
-        return List.of();
+
+        List<Topico> topicoList = topicoRepository.findAll();
+        List<DatosTopico> datosTopicoList = new ArrayList<>();
+
+        topicoList.forEach(topico -> datosTopicoList.add(modelMapper.map(topico, DatosTopico.class)));
+
+
+        return datosTopicoList;
     }
 
     @Override
     public DatosTopico findByIdTopico(Long id) {
-        return null;
+        return modelMapper.map(topicoRepository.findById(id), DatosTopico.class);
     }
 
     @Override
     public void addTopico(DatosTopico datosTopico) {
-
+        topicoRepository.save(modelMapper.map(datosTopico, Topico.class));
     }
 
     @Override
     public void updateTopico(DatosTopico datosTopico, Long id) {
-
+        Topico topico = modelMapper.map(datosTopico, Topico.class);
+        topico.setId(id);
+        topicoRepository.save(topico);
     }
 
     @Override
     public void deleteTopico(Long id) {
-
+        topicoRepository.deleteById(id);
     }
 }
